10c457e1a7e34fdef1ed99c8b089e292c95363de
Damiano Galassi <damiog@gmail.com>
2026-02-15 15:32:09 +0100

    Update Package management files for version 2.8.1-handbrake

--- Package.swift
+++ Package.swift
@@ -4,9 +4,9 @@ import PackageDescription
 // Version is technically not required here, SPM doesn't check
 let version = "2.8.1"
 // Tag is required to point towards the right asset. SPM requires the tag to follow semantic versioning to be able to resolve it.
-let tag = "2.8.1"
-let checksum = "995f913935fe903d50b3be5344fc52a638f56b39ef7984bf7a44244786d28638"
-let url = "https://github.com/sparkle-project/Sparkle/releases/download/\(tag)/Sparkle-for-Swift-Package-Manager.zip"
+let tag = "2.8.1-handbrake"
+let checksum = "649c3fe44a491a13153f7fd1e7cb79266bb71e461eabf48d65467dda3bf31a70"
+let url = "https://github.com/HandBrake/Sparkle/releases/download/\(tag)/Sparkle-for-Swift-Package-Manager.zip"
 
 let package = Package(
     name: "Sparkle",
